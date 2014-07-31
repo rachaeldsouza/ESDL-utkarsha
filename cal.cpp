b2303dc0ac65d4dd8d8de2f500a49b526357cdd0
rachaeldsouza <rachaeldsouza30@gmail.com>
2014-07-31 10:51:16 +0530

    Update cal.cpp
    added new cube function

--- cal.cpp
+++ cal.cpp
@@ -5,10 +5,16 @@ using namespace std;
  void main()
  {
    cout<<"Welcome to github";
-   
+   cout<<"Square(10)";
+   cout<<"Cube(12)";
  }
  
  double square(in_no)
  {
   return in_no*in_no;
  }
+
+double cube(in_no)
+{
+ return in_no*in_no*in_no;
+}
